f9e6f5bed6448f3b42380705a28f06b0c4a8aa80
camilled671 <72838912+camilled671@users.noreply.github.com>
2021-01-14 22:39:01 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 #while True:
-   #    print"input.sound_level()"
-   # if input.sound_level()>5:
-      #  light.set_all(light.rgb(255, 0, 255))
+    #print("sound level :" +input.sound_level())
+    #if input.sound_level()>5:
+            #light.set_all(light.rgb(255, 0, 255))
+    #else:
+        #light.set_all(light.rgb(255, 0, 255))  
+#while True:
+    #print("light level: " + str(input.light_level()))
+    #if input.light_level() >11:
+        #music.power_down.play_until_done()
+    #else:
+        #music.stop_all_sounds()
+music.play_melody("E D C D E E E D D D E G G E D C D E E E E D E D C ", 120)
